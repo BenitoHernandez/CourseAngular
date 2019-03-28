aa9a098632ca04e6bb3e2994cbce1249c945d40b
Benito Hernandez <benito.hernandez@nearshoretechnology.com>
2019-03-28 11:55:37 -0600

    Services and observables

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../../services/hero.service';
  
 @Component({
   selector: 'app-heroes',
@@ -13,9 +14,15 @@ export class HeroesComponent implements OnInit {
   heroes = HEROES;
   selectedHero: Hero;
  
-  constructor() { }
- 
+  constructor(private heroService: HeroService) { } 
+
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
  
   onSelect(hero: Hero): void {
